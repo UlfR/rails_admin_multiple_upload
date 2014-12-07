7e5ea5b15e8fcc35fa10db633e481bfab25f5d94
Gleb Stiblo <errandir@gmail.com>
2014-12-07 22:51:45 +0600

    file upload

--- lib/rails_admin_multiple_upload.rb
+++ lib/rails_admin_multiple_upload.rb
@@ -7,49 +7,44 @@ end
 require 'rails_admin/config/actions'
 
 module RailsAdmin
-  module Config
-    module Actions
-      class MultipleUpload < Base
-        RailsAdmin::Config::Actions.register(self)
-        register_instance_option :member do
-          true
-        end
+	module Config
+		module Actions
+			class MultipleUpload < Base
+				RailsAdmin::Config::Actions.register(self)
+				register_instance_option :member do
+					true
+				end
 
-        register_instance_option :link_icon do
-          'icon-upload'
-        end
+				register_instance_option :link_icon do
+					'icon-upload'
+				end
 
-        register_instance_option :http_methods do
-          [:get, :post]
-        end
+				register_instance_option :http_methods do
+					[:get, :post]
+				end
 
-        register_instance_option :controller do
-          Proc.new do
-            @response = {}
+				register_instance_option :controller do
+					Proc.new do
+						@response = {}
 
-            if request.post?
-              satisfy_strong_params!
-              destenation = "#{@object.nested_attributes_options.map{|k, v| k}.first}_attributes"
-              @object.update_attribute(destenation.to_sym, params[@object.class.name.downcase.to_sym][destenation.to_sym])
+						if request.post?
+							dest   = "#{@object.nested_attributes_options.map{|k, v| k}.first}_attributes"
+							dest_s = dest_s.to_sym
 
-              #obj = Model.find_by_id(params[:id])
-              #obj.update_attributes(attributes)
+							files  = params[@object.class.name.downcase.to_sym][dest_s]
+							files.each do | file |
+								@object.docs << Doc.create(data: file[:file])
+							end
 
-              #@album = Album.new(params[:album])
-              #if @album.save
-              #redirect_to action: "index"
-              #end
-
-              #results = @abstract_model.model.run_import(params)
-
-              #@response[:notice] = results[:success].join("<br />").html_safe if results[:success].any?
-              #@response[:error] = results[:error].join("<br />").html_safe if results[:error].any?
-            end
-
-            render :action => @action.template_name
-          end
-        end
-      end
-    end
-  end
+							redirect_to action: "index"
+							#@response[:notice] = results[:success].join("<br />").html_safe if results[:success].any?
+							#@response[:error] = results[:error].join("<br />").html_safe if results[:error].any?
+						else
+							render :action => @action.template_name
+						end
+					end
+				end
+			end
+		end
+	end
 end
